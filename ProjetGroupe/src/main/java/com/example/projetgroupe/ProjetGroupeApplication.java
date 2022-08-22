c600c928d04bc36f9f155e6b4ab1fec935174f0a
carolinebiegel <caroline.biegel24@gmail.com>
2022-08-22 10:22:49 +0200

    Essai
    essai

---
+++ ProjetGroupe/src/main/java/com/example/projetgroupe/ProjetGroupeApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetgroupe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetGroupeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetGroupeApplication.class, args);
+    }
+
+}
---
+++ ProjetGroupe/src/test/java/com/example/projetgroupe/ProjetGroupeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetgroupe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetGroupeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
