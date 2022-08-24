1e01388901a74ae7d6d5b35f5962e42d1e32c7ba
Quentin Chacon <106095955+quentinChacon@users.noreply.github.com>
2022-08-24 17:15:34 +0200

    route profil

--- ProjetGroupe/src/main/java/com/example/projetgroupe/controller/AvisController.java
+++ ProjetGroupe/src/main/java/com/example/projetgroupe/controller/AvisController.java
@@ -1,13 +1,17 @@
 package com.example.projetgroupe.controller;
 
 import com.example.projetgroupe.bo.Avis;
+import com.example.projetgroupe.bo.Membres;
 import com.example.projetgroupe.service.AvisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class AvisController {
@@ -27,6 +31,27 @@ public class AvisController {
         model.addAttribute("listeAvis", avisService.listeAvis());
         return "redirect:/admin/avisMembre";
     }
+    @PostMapping("/admin/profil")
+    private String postAddAvis(@Valid Avis avis, BindingResult br, Model model) {
 
+        // si on a des erreurs de validations, on retourne  le template pour les afficher
+        if (br.hasErrors()) {
+            model.addAttribute("listeAvis", avisService.listeAvis());
+            return "creationAvis";
+        }
+
+        // creer le membre via membreService
+        try {
+            avisService.addAvis(avis);
+        }
+        // si jamais ca se passe mal
+        catch (Exception e) {
+            // on ajoute un attribut "erreur" au modèle
+            model.addAttribute("erreur", e.getMessage());
+            model.addAttribute("listeMembres", avisService.listeAvis());
+            return "Profil";
+        }
+        return "redirect:/";
+    }
 
 }
